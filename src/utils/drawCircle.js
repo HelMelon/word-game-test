94345dccb1f19d7ec5fd7ef083cf19df266f9a3b
HelMelon <helgamelon@yandex.ru>
2024-07-05 14:08:30 +0300

    fix

--- src/utils/drawCircle.js
+++ src/utils/drawCircle.js
@@ -3,7 +3,7 @@ export function drawCircle(arr) {
     for (let i = 0, j = calculation; i <= arr.length; i++, j = j + calculation) {
         document.querySelectorAll(`.letter:nth-child(${i})`).forEach((elem) => {
             elem.style.cssText = `
-                transform: rotate(${j}deg) translate(-${calculation / 2}px, ${calculation / 2}px);
+                transform: rotate(${j}deg) translate(-${calculation}px, ${calculation / 2}px);
             `;
 
             document.querySelectorAll(`.letter:nth-child(${i}) p`).forEach(elem => {
